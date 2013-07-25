58811578a156d7c6fbf0c29be4dfd6c0f3161bd6
Ben Sklaroff <bsklaroff@gmail.com>
2013-07-24 17:41:23 -0700

    add rewrite of query expressions and finding dependency info for bloom statements

--- parser/bloom_nodes.js
+++ parser/bloom_nodes.js
@@ -6,9 +6,8 @@ var isArray = function(x) {
   return getClass.call(x) === '[object Array]';
 };
 
-var Base = function() {
-};
-
+var Base = function() {};
+Base.prototype.kind = 'Base';
 Base.prototype.children = [];
 Base.prototype.traverseWhile = function(fn) {
   var children, self = this;
@@ -31,12 +30,24 @@ Base.prototype.traverseWhile = function(fn) {
     });
   }
 };
+Base.prototype.genJSCode = function() {
+  return '(' + this.type + ' UNIMPLEMENTED)';
+};
+Base.prototype.genSQLCode = function() {
+  return '(' + this.type + ' UNIMPLEMENTED)';
+};
+
+var QueryExpr = function() {};
+QueryExpr.prototype = new Base();
+QueryExpr.prototype.kind = 'QueryExpr';
+QueryExpr.prototype.monotonic = true;
+QueryExpr.prototype.collections = function() { return []; };
 
 var Program = function(statements) {
-  this.type = 'Program';
   this.statements = statements;
 };
 Program.prototype = new Base();
+Program.prototype.type = 'Program';
 Program.prototype.children = ['statements'];
 Program.prototype.genJSCode = function() {
   var res = "var Bloom = require('./Bloom');\n";
@@ -55,12 +66,12 @@ Program.prototype.genSQLCode = function() {
 exports.Program = Program;
 
 var ClassBlock = function(name, statements) {
-  this.type = 'ClassBlock';
   this.name = name;
   this.statements = statements;
   this.stateInfo = null;
 };
 ClassBlock.prototype = new Base();
+ClassBlock.prototype.type = 'ClassBlock';
 ClassBlock.prototype.children = ['statements'];
 ClassBlock.prototype.genJSCode = function() {
   var res =
@@ -90,10 +101,10 @@ ClassBlock.prototype.genSQLCode = function() {
 exports.ClassBlock = ClassBlock;
 
 var StateBlock = function(stateDecls) {
-  this.type = 'StateBlock';
   this.stateDecls = stateDecls;
 };
 StateBlock.prototype = new Base();
+StateBlock.prototype.type = 'StateBlock';
 StateBlock.prototype.children = ['stateDecls'];
 StateBlock.prototype.genJSCode = function() {
   var res = this.className + '.prototype.initializeState = function() {\n';
@@ -113,11 +124,11 @@ StateBlock.prototype.genSQLCode = function() {
 exports.StateBlock = StateBlock;
 
 var BootstrapBlock = function(statements) {
-  this.type = 'BootstrapBlock';
   this.className = '';
   this.statements = statements;
 };
 BootstrapBlock.prototype = new Base();
+BootstrapBlock.prototype.type = 'BootstrapBlock';
 BootstrapBlock.prototype.children = ['statements'];
 BootstrapBlock.prototype.genJSCode = function() {
   var res = this.className + '.prototype.initializeOps = function() {\n';
@@ -140,12 +151,12 @@ BootstrapBlock.prototype.genSQLCode = function() {
 exports.BootstrapBlock = BootstrapBlock;
 
 var BloomBlock = function(name, statements) {
-  this.type = 'BloomBlock';
   this.className = '';
   this.name = name === undefined ? '' : name;
   this.statements = statements;
 };
 BloomBlock.prototype = new Base();
+BloomBlock.prototype.type = 'BloomBlock';
 BloomBlock.prototype.children = ['name', 'statements'];
 BloomBlock.prototype.genJSCode = function() {
   var res = this.className + '.prototype.initializeOps = function() {\n';
@@ -168,13 +179,13 @@ BloomBlock.prototype.genSQLCode = function() {
 exports.BloomBlock = BloomBlock;
 
 var StateDecl = function(collectionType, name, keys, vals) {
-  this.type = 'StateDecl';
   this.name = name;
   this.collectionType = collectionType;
   this.keys = keys;
   this.vals = vals;
 };
 StateDecl.prototype = new Base();
+StateDecl.prototype.type = 'StateDecl';
 StateDecl.prototype.children = ['name', 'keys', 'vals'];
 StateDecl.prototype.genJSCode = function() {
   var res = 'this.addCollection(' + this.name.genJSCode() + ', ' + this.type +
@@ -216,34 +227,31 @@ StateDecl.prototype.genSQLCode = function() {
 };
 exports.StateDecl = StateDecl;
 
-var BloomStmt = function(destCollection, bloomOp, srcCollection) {
-  this.type = 'BloomStmt';
+var BloomStmt = function(targetCollection, bloomOp, queryExpr) {
   this.opPrefix = 'this';
   this.bloomOp = bloomOp;
-  this.destCollection = destCollection;
-  this.srcCollection = srcCollection;
+  this.targetCollection = targetCollection;
+  this.queryExpr = queryExpr;
   this.dependencyInfo = null;
-  this.target = null;
-  this.monotonicDeps = [];
-  this.nonMonotonicDeps = [];
 };
 BloomStmt.prototype = new Base();
-BloomStmt.prototype.children = ['destCollection', 'srcCollection'];
+BloomStmt.prototype.type = 'BloomStmt';
+BloomStmt.prototype.children = ['targetCollection', 'queryExpr'];
 BloomStmt.prototype.genJSCode = function() {
   var res = this.opPrefix + '.op(' + this.bloomOp + ',' +
-    this.destCollection.genJSCode() + ',' + this.srcCollection.genJSCode()
-  + ', { target: ' + this.target + ', monotonicDeps: [';
-  this.monotonicDeps.forEach(function(dep) {
+    this.targetCollection.genJSCode() + ',' + this.queryExpr.genJSCode()
+  + ', { target: ' + this.dependencyInfo.target + ', monotonicDeps: [';
+  this.dependencyInfo.monotonicDeps.forEach(function(dep) {
     res += dep + ', ';
   });
-  if (this.monotonicDeps.length > 0) {
+  if (this.dependencyInfo.monotonicDeps.length > 0) {
     res = res.slice(0, -2);
   }
   res += '], nonMonotonicDeps: ['
-  this.nonMonotonicDeps.forEach(function(dep) {
+  this.dependencyInfo.nonMonotonicDeps.forEach(function(dep) {
     res += dep + ', ';
   });
-  if (this.nonMonotonicDeps.length > 0) {
+  if (this.dependencyInfo.nonMonotonicDeps.length > 0) {
     res = res.slice(0, -2);
   }
   res += '] });\n';
@@ -256,10 +264,10 @@ BloomStmt.prototype.genSQLCode = function() {
 exports.BloomStmt = BloomStmt;
 
 var ExprStmt = function(expr) {
-  this.type = 'ExprStmt';
   this.expr = expr;
 };
 ExprStmt.prototype = new Base();
+ExprStmt.prototype.type = 'ExprStmt';
 ExprStmt.prototype.children = ['expr'];
 ExprStmt.prototype.genJSCode = function() {
   return this.expr.genJSCode() + ';\n';
@@ -267,11 +275,11 @@ ExprStmt.prototype.genJSCode = function() {
 exports.ExprStmt = ExprStmt;
 
 var AssignmentStmt = function(target, value) {
-  this.type = 'AssignmentStmt';
   this.target = target;
   this.value = value;
 };
 AssignmentStmt.prototype = new Base();
+AssignmentStmt.prototype.type = 'AssignmentStmt';
 AssignmentStmt.prototype.children = ['target', 'value'];
 AssignmentStmt.prototype.genJSCode = function() {
   return 'var ' + this.target.genJSCode() + ' = ' + this.value.genJSCode() +
@@ -283,11 +291,11 @@ AssignmentStmt.prototype.genSQLCode = function() {
 exports.AssignmentStmt = AssignmentStmt;
 
 var AssignmentStmtNoDecl = function(target, value) {
-  this.type = 'AssignmentStmtNoDecl';
   this.target = target;
   this.value = value;
 };
 AssignmentStmtNoDecl.prototype = new Base();
+AssignmentStmtNoDecl.prototype.type = 'AssignmentStmtNoDecl';
 AssignmentStmtNoDecl.prototype.children = ['target', 'value'];
 AssignmentStmtNoDecl.prototype.genJSCode = function() {
   return this.target.genJSCode() + ' = ' + this.value.genJSCode() + ';\n';
@@ -295,10 +303,10 @@ AssignmentStmtNoDecl.prototype.genJSCode = function() {
 exports.AssignmentStmtNoDecl = AssignmentStmtNoDecl;
 
 var PutsStmt = function(values) {
-  this.type = 'PutsStmt';
   this.values = values;
 };
 PutsStmt.prototype = new Base();
+PutsStmt.prototype.type = 'PutsStmt';
 PutsStmt.prototype.children = ['values'];
 PutsStmt.prototype.genJSCode = function() {
   var res = 'console.log(';
@@ -314,12 +322,12 @@ PutsStmt.prototype.genJSCode = function() {
 exports.PutsStmt = PutsStmt;
 
 var TernaryExpr = function(cond, thenExpr, elseExpr) {
-  this.type = 'TernaryExpr';
   this.cond = cond;
   this.thenExpr = thenExpr;
   this.elseExpr = elseExpr;
 };
 TernaryExpr.prototype = new Base();
+TernaryExpr.prototype.type = 'TernaryExpr';
 TernaryExpr.prototype.children = ['cond', 'thenExpr', 'elseExpr'];
 TernaryExpr.prototype.genJSCode = function() {
   return this.cond.genJSCode() + ' ? ' + this.thenExpr.genJSCode() + ' : ' +
@@ -328,12 +336,12 @@ TernaryExpr.prototype.genJSCode = function() {
 exports.TernaryExpr = TernaryExpr;
 
 var Binop = function(left, op, right) {
-  this.type = 'Binop';
   this.op = op;
   this.left = left;
   this.right = right;
 };
 Binop.prototype = new Base();
+Binop.prototype.type = 'Binop';
 Binop.prototype.children = ['left', 'right'];
 Binop.prototype.genJSCode = function() {
   return this.left.genJSCode() + ' ' + this.op + ' ' + this.right.genJSCode();
@@ -341,11 +349,11 @@ Binop.prototype.genJSCode = function() {
 exports.Binop = Binop;
 
 var Unop = function(op, value) {
-  this.type = 'Unop';
   this.op = op;
   this.value = value;
 };
 Unop.prototype = new Base();
+Unop.prototype.type = 'Unop';
 Unop.prototype.children = ['op', 'value'];
 Unop.prototype.genJSCode = function() {
   return this.op + ' ' + this.right;
@@ -353,10 +361,10 @@ Unop.prototype.genJSCode = function() {
 exports.Unop = Unop;
 
 var VarName = function(name) {
-  this.type = 'VarName';
   this.name = name;
 };
 VarName.prototype = new Base();
+VarName.prototype.type = 'VarName';
 VarName.prototype.children = [];
 VarName.prototype.genJSCode = function() {
   return this.name;
@@ -364,21 +372,21 @@ VarName.prototype.genJSCode = function() {
 exports.VarName = VarName;
 
 var StrLiteral = function(raw) {
-  this.type = 'StrLiteral';
   this.raw = raw;
   this.value = raw.slice(1, -1);
 };
 StrLiteral.prototype = new Base();
+StrLiteral.prototype.type = 'StrLiteral';
 StrLiteral.prototype.genJSCode = function() {
-  return this.value;
+  return this.raw;
 };
 exports.StrLiteral = StrLiteral;
 
 var NumLiteral = function(value) {
-  this.type = 'NumLiteral';
   this.value = value;
 };
 NumLiteral.prototype = new Base();
+NumLiteral.prototype.type = 'NumLiteral';
 NumLiteral.prototype.children = [];
 NumLiteral.prototype.genJSCode = function() {
   return this.value;
@@ -386,10 +394,10 @@ NumLiteral.prototype.genJSCode = function() {
 exports.NumLiteral = NumLiteral;
 
 var ArrDisplay = function(arr) {
-  this.type = 'ArrDisplay';
   this.arr = arr;
 };
 ArrDisplay.prototype = new Base();
+ArrDisplay.prototype.type = 'ArrDisplay';
 ArrDisplay.prototype.children = ['arr'];
 ArrDisplay.prototype.genJSCode = function() {
   res = '[';
@@ -405,10 +413,10 @@ ArrDisplay.prototype.genJSCode = function() {
 exports.ArrDisplay = ArrDisplay;
 
 var HashDisplay = function(kvPairs) {
-  this.type = 'HashDisplay';
   this.kvPairs = kvPairs;
 };
 HashDisplay.prototype = new Base();
+HashDisplay.prototype.type = 'HashDisplay';
 HashDisplay.prototype.children = function() {
   return [].concat.apply([], this.kvPairs);
 };
@@ -426,11 +434,11 @@ HashDisplay.prototype.genJSCode = function() {
 exports.HashDisplay = HashDisplay;
 
 var AttributeRef = function(obj, attribute) {
-  this.type = 'AttributeRef';
   this.obj = obj;
   this.attribute = attribute;
 };
 AttributeRef.prototype = new Base();
+AttributeRef.prototype.type = 'AttributeRef';
 AttributeRef.prototype.children = ['obj', 'attribute'];
 AttributeRef.prototype.genJSCode = function() {
   return this.obj.genJSCode() + '.' + this.attribute.genJSCode();
@@ -438,11 +446,11 @@ AttributeRef.prototype.genJSCode = function() {
 exports.AttributeRef = AttributeRef;
 
 var Subscription = function(obj, subscription) {
-  this.type = 'Subscription';
   this.obj = obj;
   this.subscription = subscription;
 };
 Subscription.prototype = new Base();
+Subscription.prototype.type = 'Subscription';
 Subscription.prototype.children = ['obj', 'subscription'];
 Subscription.prototype.genJSCode = function() {
   return this.obj.genJSCode() + '[' + this.subscription.genJSCode() + ']';
@@ -450,11 +458,11 @@ Subscription.prototype.genJSCode = function() {
 exports.Subscription = Subscription;
 
 var Call = function(func, args) {
-  this.type = 'Call';
   this.func = func;
   this.args = args;
 };
 Call.prototype = new Base();
+Call.prototype.type = 'Call';
 Call.prototype.children = ['func', 'args'];
 Call.prototype.genJSCode = function() {
   var res = this.func.genJSCode() + '(';
@@ -470,11 +478,11 @@ Call.prototype.genJSCode = function() {
 exports.Call = Call;
 
 var NewExpr = function(name, args) {
-  this.type = 'NewExpr';
   this.name = name;
   this.args = args;
 };
 NewExpr.prototype = new Base();
+NewExpr.prototype.type = 'NewExpr';
 NewExpr.prototype.children = ['args'];
 NewExpr.prototype.genJSCode = function() {
   res = 'new ' + this.name + '(';
@@ -490,11 +498,11 @@ NewExpr.prototype.genJSCode = function() {
 exports.NewExpr = NewExpr;
 
 var PrimaryBlock = function(primary, funcExpr) {
-  this.type = 'PrimaryBlock';
   this.primary = primary;
   this.funcExpr = funcExpr;
 };
 PrimaryBlock.prototype = new Base();
+PrimaryBlock.prototype.type = 'PrimaryBlock';
 PrimaryBlock.prototype.children = ['primary', 'funcExpr'];
 PrimaryBlock.prototype.genJSCode = function() {
   return this.primary.genJSCode() + '(' + this.funcExpr.genJSCode() + ')';
@@ -502,11 +510,11 @@ PrimaryBlock.prototype.genJSCode = function() {
 exports.PrimaryBlock = PrimaryBlock;
 
 var FuncExpr = function(args, statements) {
-    this.type = 'FuncExpr';
     this.args = args;
     this.statements = statements;
 };
 FuncExpr.prototype = new Base();
+FuncExpr.prototype.type = 'FuncExpr';
 FuncExpr.prototype.children = ['args', 'statements'];
 FuncExpr.prototype.genJSCode = function() {
   res = 'function(';
@@ -529,12 +537,12 @@ FuncExpr.prototype.genJSCode = function() {
 exports.FuncExpr = FuncExpr;
 
 var IfStmt = function(cond, thenStmts, elseClause) {
-  this.type = 'IfStmt';
   this.cond = cond;
   this.thenStmts = thenStmts;
   this.elseClause = elseClause;
 };
 IfStmt.prototype = new Base();
+IfStmt.prototype.type = 'IfStmt';
 IfStmt.prototype.children = ['cond', 'thenStmts', 'elseClause'];
 IfStmt.prototype.genJSCode = function() {
   res = 'if (' + this.cond.genJSCode() + ') {\n';
@@ -547,12 +555,12 @@ IfStmt.prototype.genJSCode = function() {
 exports.IfStmt = IfStmt;
 
 var ElseIfClause = function(cond, thenStmts, elseClause) {
-  this.type = 'ElseIfClause';
   this.cond = cond;
   this.thenStmts = thenStmts;
   this.elseClause = elseClause;
 };
 ElseIfClause.prototype = new Base();
+ElseIfClause.prototype.type = 'ElseIfClause';
 ElseIfClause.prototype.children = ['cond', 'thenStmts', 'elseClause'];
 ElseIfClause.prototype.genJSCode = function() {
   res = ' else if (' + this.cond.genJSCode() + ') {\n';
@@ -565,10 +573,10 @@ ElseIfClause.prototype.genJSCode = function() {
 exports.ElseIfClause = ElseIfClause;
 
 var ElseClause = function(statements) {
-  this.type = 'ElseClause';
   this.statements = statements;
 };
 ElseClause.prototype = new Base();
+ElseClause.prototype.type = 'ElseClause';
 ElseClause.prototype.children = ['cond', 'statements'];
 ElseClause.prototype.genJSCode = function() {
   if (this.statements === null) {
@@ -581,3 +589,67 @@ ElseClause.prototype.genJSCode = function() {
   return res;
 };
 exports.ElseClause = ElseClause;
+
+var ValuesExpr = function(values) {
+  this.values = values;
+};
+ValuesExpr.prototype = new QueryExpr();
+ValuesExpr.prototype.type = 'ValuesExpr';
+ValuesExpr.prototype.children = ['values'];
+ValuesExpr.prototype.genJSCode = function() {
+  return 'values(' + this.values.genJSCode() + ')';
+};
+exports.ValuesExpr = ValuesExpr;
+
+var SelectExpr = function(collectionName, selectFn) {
+  this.collectionName = collectionName;
+  this.selectFn = selectFn;
+};
+SelectExpr.prototype = new QueryExpr();
+SelectExpr.prototype.type = 'SelectExpr';
+SelectExpr.prototype.collections = function() {
+  return [this.collectionName];
+};
+SelectExpr.prototype.children = ['selectFn'];
+SelectExpr.prototype.genJSCode = function() {
+  return this.collectionName + '.select(' + this.selectFn.genJSCode() + ')';
+};
+exports.SelectExpr = SelectExpr;
+
+var JoinExpr = function(leftCol, rightCol, leftKeys, rightKeys, joinFn) {
+  this.leftCollectionName = leftCol;
+  this.rightCollectionName = rightCol;
+  this.leftJoinKeys = leftKeys;
+  this.rightJoinKeys = rightKeys;
+  this.joinFn = joinFn;
+};
+JoinExpr.prototype = new QueryExpr();
+JoinExpr.prototype.type = 'JoinExpr';
+JoinExpr.prototype.collections = function() {
+  return [this.leftCollectionName, this.rightCollectionName];
+};
+JoinExpr.prototype.children = ['leftKeyFn', 'rightKeyFn', 'joinFn'];
+JoinExpr.prototype.genJSCode = function() {
+  return this.leftCollectionName + '.join(' + this.rightCollectionName + ', ' +
+    this.leftJoinKeys.genJSCode() + ', ' + this.rightJoinKeys.genJSCode() +
+    ', ' + this.joinFn.genJSCode() + ')';
+}
+exports.JoinExpr = JoinExpr;
+
+var ArgminExpr = function(collectionName, groupKeys, minKey) {
+  this.collectionName = collectionName;
+  this.groupKeys = groupKeys;
+  this.minKey = minKey;
+};
+ArgminExpr.prototype = new QueryExpr();
+ArgminExpr.prototype.type = 'ArgminExpr';
+ArgminExpr.prototype.monotonic = false;
+ArgminExpr.prototype.collections = function() {
+  return [this.collectionName];
+};
+ArgminExpr.prototype.children = ['groupKeys', 'minKey'];
+ArgminExpr.prototype.genJSCode = function() {
+  return this.collectionName + '.argmin(' + this.groupKeys.genJSCode() + ', ' +
+    this.minKey.genJSCode() + ')';
+};
+exports.ArgminExpr = ArgminExpr;
--- parser/bloom_rewriter.js
+++ parser/bloom_rewriter.js
@@ -1,5 +1,39 @@
 var nodes = require('./bloom_nodes');
 
+var rewriteQueryExpr = function(qe) {
+  var primary, fnName, leftCollectionName, rightCollectionName, leftKeysArr,
+    rightKeysArr, res = qe;
+  if (qe.type === 'ArrDisplay') {
+    res = new nodes.ValuesExpr(qe);
+  } else if (qe.type === 'PrimaryBlock') {
+    if (qe.primary.type === 'VarName') {
+      res = new nodes.SelectExpr(qe.primary.name, qe.funcExpr);
+    } else if (qe.primary.type === 'Call') {
+      fnName = qe.primary.func.attribute.name;
+      if (fnName === 'pairs') {
+        leftCollectionName = qe.primary.func.obj.left.name;
+        rightCollectionName = qe.primary.func.obj.right.name;
+        leftKeysArr = [];
+        rightKeysArr = [];
+        qe.primary.args[0].kvPairs.forEach(function(kvPair) {
+          leftKeysArr.push(kvPair[0]);
+          rightKeysArr.push(kvPair[1]);
+        });
+        res = new nodes.JoinExpr(leftCollectionName, rightCollectionName,
+                                 new nodes.ArrDisplay(leftKeysArr),
+                                 new nodes.ArrDisplay(rightKeysArr),
+                                 qe.funcExpr);
+      }
+    }
+  } else if (qe.type === 'Call' && qe.func.type === 'AttributeRef') {
+    fnName = qe.func.attribute.name;
+    if (fnName === 'argmin') {
+      res = new nodes.ArgminExpr(qe.func.obj.name, qe.args[0], qe.args[1]);
+    }
+  }
+  return res;
+};
+
 var getStateInfo = function(classBlock) {
   var res = {}, keys, vals;
   classBlock.statements.forEach(function(statement) {
@@ -23,7 +57,16 @@ var getStateInfo = function(classBlock) {
 };
 
 var getDependencyInfo = function(bloomStmt) {
-  var res = {};
+  var res = {
+    target: bloomStmt.targetCollection.name,
+    monotonicDeps: [],
+    nonMonotonicDeps: []
+  };
+  if (bloomStmt.queryExpr.monotonic) {
+    res.monotonicDeps = bloomStmt.queryExpr.collections();
+  } else {
+    res.nonMonotonicDeps = bloomStmt.queryExpr.collections();
+  }
   return res;
 };
 
@@ -31,8 +74,11 @@ exports.rewrite = function(ast) {
   ast.traverseWhile(function(node) {
     if (node.type === 'ClassBlock') {
       node.stateInfo = getStateInfo(node);
-    } else if (node.type === 'BloomStmt' && node.bloomOp === '<=') {
-      node.dependencyInfo = getDependencyInfo(node);
+    } else if (node.type === 'BloomStmt') {
+      node.queryExpr = rewriteQueryExpr(node.queryExpr);
+      if (node.bloomOp === '<=') {
+        node.dependencyInfo = getDependencyInfo(node);
+      }
       return false;
     }
     return true;
